cfbe8ae1dd615725b070e2199005f5f47617f895
Bhalerao.Nayan <bhalerao.nayan899@gmail.com>
2022-04-16 19:04:55 +0530

    everything for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'you have successfuly loged in '
+      redirect_to root_path
+
+    else
+      flash.now[:error] = 'please enter some valid information'
+      render 'new'
+    end
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash.now[:success]= 'you have log out successfully'
+    redirect_to login_path
+end
 end
