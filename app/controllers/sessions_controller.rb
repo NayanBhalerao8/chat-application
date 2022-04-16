a0eb42ed76ab9d0aa79fd4aa8c538c04a975ac47
Nayan Bhalerao <rubydev.nayan@gmail.com>
2022-04-16 19:59:46 +0530

    controller level restrictions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  def new; end
+  before_action :logged_in_redirect, only: [:new, :create]
+  def new
+  end
 
   def create
     user = User.find_by(username: params[:session][:username])
@@ -21,4 +23,15 @@ class SessionsController < ApplicationController
     flash.now[:success]= 'you have log out successfully'
     redirect_to login_path
 end
+
+
+private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
+  end
+
 end
