b6f1180d730d6967a93824673a2f4f08e5c396c2
Nayan Bhalerao <rubydev.nayan@gmail.com>
2022-04-16 16:37:34 +0530

    created the ui added user and message model and started making it dynamic

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username:"nayan",password:"nayan123")
+User.create(username:"gauri",password:"gauri123")
+User.create(username:"kalpesh",password:"kalpesh123")
+User.create(username:"example1",password:"example123")
+User.create(username:"exampl2",password:"ekhhmk")
+User.create(username:"account3",password:"okkkkk")
+
